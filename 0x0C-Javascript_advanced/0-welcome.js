7a26e0511c86fd99a1f3f1317308bbe5206c509c
maycolroa <lehallo@hotmail.com>
2022-09-02 22:30:09 -0500

    0x0C. JavaScript advanced  02/09/22 0.00

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,7 @@
+function welcome(firstname = "", lastname = "") {
+    let fullname = firstname + " " + lastname;
+    function displayFullName() {
+        alert("Welcome " + fullname + "!");
+    }
+    displayFullName();
+}
